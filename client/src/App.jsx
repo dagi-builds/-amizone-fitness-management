e2c672cbfeffa332136cff458227e2cf1fcfeeee
Abdi Aliko <abaliko82@gmail.com>
2026-06-28 17:53:19 +0300

    Add vercel.json for backend deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Footer from './components/shared/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
 import AdminDashboard from './pages/AdminDashboard'
 
+function PublicLayout({ children }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
 export default function App() {
-  return <AdminDashboard />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicLayout><Home /></PublicLayout>} />
+        <Route path="/about" element={<PublicLayout><About /></PublicLayout>} />
+        <Route path="/services" element={<PublicLayout><Services /></PublicLayout>} />
+        <Route path="/contact" element={<PublicLayout><Contact /></PublicLayout>} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
