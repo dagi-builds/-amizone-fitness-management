e1298488b8295c2afbf95b8c06f88790897cb840
Abdi Aliko <abaliko82@gmail.com>
2026-07-16 13:41:51 +0300

    Export app for Vercel serverless handler, only listen locally

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Amizone Fitness server running on http://localhost:${PORT}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Amizone Fitness server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
